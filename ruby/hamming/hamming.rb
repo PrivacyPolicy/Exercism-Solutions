43162029ac905db98740337955d89c35a6af2cb9
Gabriel Hutchison <ghutchison2600@flpoly.org>
2017-02-05 19:44:31 -0500

    Passing hamming B

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,7 @@
 class Hamming
     def self.compute(a, b)
+        throw :ArgumentError if a.length != b.length
+        
         distance = 0
         for i in 0..(a.length)
             if a[i] != b[i]
