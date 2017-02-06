375107bf264935018f80336dd4cb4abd2725fc19
Gabriel Hutchison <ghutchison2600@flpoly.org>
2017-02-06 00:37:57 -0500

    Passing difference-of-squares B

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -4,17 +4,20 @@ end
 
 class Squares
     
-    @@square_of_sum = 0
-    @@sum_of_squares = 0
-    
     def self.new(number)
+        @@square_of_sum = 0
+        @@sum_of_squares = 0
+        self.new_rec number
+    end
+    
+    def self.new_rec(number)
         if number <= 0
             @@square_of_sum = @@square_of_sum ** 2
             self
         else
             @@square_of_sum += number
             @@sum_of_squares += number ** 2
-            self.new number - 1
+            self.new_rec number - 1
         end
     end
     
@@ -27,6 +30,10 @@ class Squares
     end
     
     def self.difference
-        @@sum_of_squares - @@square_of_sum
+        if @@square_of_sum < @@sum_of_squares
+            @@sum_of_squares - @@square_of_sum
+        else
+            @@square_of_sum  - @@sum_of_squares
+        end
     end
 end
