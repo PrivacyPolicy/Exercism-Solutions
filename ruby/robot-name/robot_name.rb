b2e8e0bd0e8bb84f79ec57b7b3cf13e45a11ef93
Gabriel Hutchison <ghutchison2600@flpoly.org>
2017-02-19 13:27:11 -0500

    Passing robot_name A

---
+++ ruby/robot-name/robot_name.rb
@@ -0,0 +1,21 @@
+module BookKeeping
+    VERSION = 2
+end
+
+class Robot
+    
+    @@name = ''
+    
+    def self.new
+        rand = Random.new_seed
+        a = rand % 26 + 65
+        b = (rand / 10) % 26 + 65
+        c = rand % 1000
+        @@name = a.chr + b.chr + c.to_s
+        self
+    end
+    
+    def self.name
+        @@name
+    end
+end
